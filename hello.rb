d8d67e828343fc9dbccc8144e399f80c650b83c4
miraisi <x8miraisi@gmail.com>
2013-12-03 03:58:21 +0900

    charset=utf-8

--- hello.rb
+++ hello.rb
@@ -2,8 +2,9 @@
 
 require 'rack'
 
+
 class HelloApp
 	def call(env)
-		[200, {"Content-Type" => "text/html"}, ['<!--?xml version="1.0" encoding="UTF-8" ?--><html><body><h1>はろはろ〜</h1></body></html>']]
+		[200, {"Content-Type" => "text/html"}, ['<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><html><body><h1>はろはろ〜</h1></body></html>']]
 	end
 end
